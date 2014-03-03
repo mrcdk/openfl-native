de7d7d1bbe89b0fb08c3761f06db74cf47f4b9e4
mrcdk <codecdakiller@gmail.com>
2014-03-04 00:12:08 +0100

    Add ID variable to GPUProgram

--- openfl/display/GPUProgram.hx
+++ openfl/display/GPUProgram.hx
@@ -3,11 +3,11 @@ import flash.Lib;
 
 class GPUProgram {
 
+	public var ID(get, null):Int;
 	public var vertexString:String;
 	public var fragmentString:String;
 	
 	@:noCompletion public var __handle:Dynamic;
-	@:noCompletion public var __id:Int;
 	
 	public function new(vertexString:String, fragmentString:String) {
 		__handle = lime_gpuprogram_create(Lib.current.stage.__handle, vertexString, fragmentString);
@@ -21,8 +21,19 @@ class GPUProgram {
 		lime_gpuprogram_use(Lib.current.stage.__handle, program.__handle);
 	}
 	
+	public function get_ID():Int {
+		var id = lime_gpuprogram_get_id(__handle);
+		if (id == null) {
+			trace("NULL ID!!");
+			return 0;
+		}
+		
+		return id;
+	}
+	
 	// Native Methods
 	
 	private static var lime_gpuprogram_create = Lib.load ("lime", "lime_gpuprogram_create", 3);
 	private static var lime_gpuprogram_use = Lib.load ("lime", "lime_gpuprogram_use", 2);
+	private static var lime_gpuprogram_get_id = Lib.load ("lime", "lime_gpuprogram_get_id", 1);
 }
